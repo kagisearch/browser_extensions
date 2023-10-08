2b1dbc39928d79b5097f317045c015b622801411
IcedRooibos <47491349+IcedRooibos@users.noreply.github.com>
2023-10-08 15:06:31 +0800

    Add right click context menu summarize feature (#34)
    * Add right click context menu summarize feature
    
    * Use activeTab permission instead of tabs
    
    * Minor mod to pass npm test
    
    * Let activeTab permission be optional
    
    * Run npm run format

--- shared/src/background.js
+++ shared/src/background.js
@@ -177,3 +177,27 @@ async function loadStorageData() {
 }
 
 loadStorageData();
+
+// The function kagiSummarize is called when clicking the context menu item.
+function kagiSummarize(info, tab) {
+  const url = info.linkUrl;
+  browser.tabs.create({
+    url: `https://kagi.com/summarizer/index.html?url=${encodeURIComponent(
+      url,
+    )}`,
+  });
+}
+
+// Create a context menu item.
+browser.contextMenus.create({
+  id: 'kagi-summarize',
+  title: 'Kagi Summarize',
+  contexts: ['link'], // Only show the menu item for links.
+});
+
+// Add a listener for the context menu item.
+browser.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === 'kagi-summarize') {
+    kagiSummarize(info, tab);
+  }
+});
