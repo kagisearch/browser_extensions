a86c521642ddc604f81ba99008428368d8cb9e75
Alabhya Jindal <alabhya10@gmail.com>
2024-02-17 10:33:10 +0530

    Add close element and handlers

--- shared/src/summarize_result.js
+++ shared/src/summarize_result.js
@@ -66,6 +66,11 @@ async function setup() {
   }
 
   summaryStatsTimeSavedElement.innerText = '0 minutes';
+  const summaryCloseElement = document.getElementById('close_summary')
+  if (!summaryCloseElement) {
+    console.error('Could not find summarize close element');
+    return;
+  }
 
   browser.runtime.onMessage.addListener(async (data) => {
     const searchParams = new URLSearchParams(window.location.search);
@@ -96,7 +101,16 @@ async function setup() {
           data.timeSavedInMinutes
         } minute${data.timeSavedInMinutes !== 1 ? 's' : ''}`;
       }
+
+      summaryCloseElement.style.display = ''
+      summaryCloseElement.addEventListener('click', () => {
+        window.close()
+      })
     }
+  });
+
+  window.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') window.close();
   });
 
   async function requestPageSummary() {
